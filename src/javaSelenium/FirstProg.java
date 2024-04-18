3b060066da3b9b4a7019afb7077b3b492d5a6443
ek-anshu <pheonix51332gmail.com>
2024-04-18 15:19:49 +0530

    third commit from Demo

--- src/javaSelenium/FirstProg.java
+++ src/javaSelenium/FirstProg.java
@@ -9,13 +9,15 @@ public class FirstProg {
 
 	public static void main(String[] args) {
 		
-		
+		//made some changes to Check Git 
 		System.setProperty("webdriver.chrome.driver", "D:\\Mission2024\\Mission2024\\Drivers\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		//driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 		driver.navigate().to("https://demoqa.com/");		
 		driver.manage().window().maximize();
 		driver.findElement(By.xpath("//h5[.='Elements']")).click();
+		System.out.println("Git Demo Change");
+		System.out.println("Git Demo Change");
 
 	}
 
